f28063d31d56ed0c9000d867c6a54935f1cb6d3b
wschiang <sinon049@yahoo.com.tw>
2021-01-20 19:34:15 +0800

    feat: add logout feature

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -40,4 +40,10 @@ router.post('/register', (req, res) => {
         .catch(err => console.log(err))
 })
 
+//logout function
+router.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/users/login')
+})
+
 module.exports = router
